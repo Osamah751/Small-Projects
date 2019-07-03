f16fb6e1884485368294faa3aabeb29da4c28fe3
Osamah Sarraj <osamah.sarraj@gmail.com>
2019-07-02 21:41:59 -0400

    Delete 2_Odd_Or_Even.py

--- Mini Project/PRACTICE PYTHON (website)/2_Odd_Or_Even.py
+++
@@ -1,7 +0,0 @@
-
-number= int(input("Enter the number you want to test: "))
-number= number % 2
-if number == 0 : # even number
-    print ("The number is even")
-else: #odd number
-    print ("The number is odd")
